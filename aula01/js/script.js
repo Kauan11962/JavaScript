c31942aafe9056eb20b7beb30b2f7df929bb3404
KAUAN VINICIUS MERONHA <0200832311041>
2024-09-19 09:28:19 -0300

    Aula 01

---
+++ aula01/js/script.js
@@ -0,0 +1,24 @@
+//alert("Olá Mundo!")
+
+var Nome = "Essa é uma variavel de texto (String)";
+var idade; // Cria uma variavel nula
+idade = 36; // Adiciona um numero inteiro na variavel (number)
+var altura = 1.81; // Variavel com numero flutuante (number)
+var diretor = false; // Variavel do tipo booleana (boolean) 
+var numeroTexto = "10";
+
+var nota1 = 10;
+var nota2 = 5;
+var media = (nota1 + nota2) / 2;
+
+var notas = [];
+notas[0] = 10;
+notas[1] = 5;
+
+var mediaArray = (notas[0] + notas[1]) / notas.length;
+
+if(numeroTexto === 10){
+    console.lo("Sâo iguais");
+} else {
+    console.log("Não são iguais");
+}
